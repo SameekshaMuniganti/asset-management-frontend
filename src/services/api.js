8369f467dd784ccfc08dae5f721a19d2b0b5a4a5
Sameeksha Muniganti <sameekshamuniganti@gmail.com>
2026-07-27 01:49:33 +0530

    Use Azure backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://asset-management-b0dmbrdehecgamcp.southindia-01.azurewebsites.net"
 });
 
 api.interceptors.request.use((config) => {
